169936f3fe8d154df0ad62d4366802759ce7064e
DarlanHildebrando <darlan_m_silva@estudante.sesisenai.org.br>
2024-08-23 17:15:43 -0300

    Criação do primeiro projeto com React

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+     <h1>Bora Bill</h1>
+     <h2>Amostradinho</h2>
+     <p>Costas e bíceps melhor treino</p>
+
+    </>
+  )
+}
+
+export default App
